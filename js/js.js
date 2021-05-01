e331187e99bad13b75d151e8c4a8f9d25c5afe23
bacharalhamada <bachar.alhmada75@gmail.com>
2021-05-01 20:47:05 +0300

    the last edit

---
+++ js/js.js
@@ -0,0 +1,25 @@
+var myVideo = document.getElementById("video1");
+function playPause() {
+    if (myVideo.paused) {
+
+        myVideo.play();
+
+    } else {
+
+        myVideo.pause();
+    }
+
+};
+
+$(document).ready(function () {
+
+
+    $("#commentForm").validate();
+
+})
+
+
+
+
+
+
